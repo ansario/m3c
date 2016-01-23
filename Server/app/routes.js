afcfeb43f0418b2987521d30e5e6b7c02efd8145
Omar Ansari <ansario@email.sc.edu>
2016-01-23 01:18:35 -0500

    Added mongo db, closes #12

---
+++ Server/app/routes.js
@@ -0,0 +1,24 @@
+
+module.exports = function(app, passport, jwt) {
+
+app.post('/login',
+      passport.authenticate('local-login'),
+      function(req, res) {
+        // If this function gets called, authentication was successful.
+        // `req.user` contains the authenticated user.
+
+        var token = jwt.sign({ user: req.user }, "DEADBODIES", { 
+                  expiresInMinutes: 1440 // expires in 24 hours
+                });
+
+        res.json({"token": token});
+        // res.redirect('/users/' + req.user.username);
+      });
+
+}
+
+app.post('/signup', passport.authenticate('local-signup'), function(req, res) {
+      res.sendStatus(200);
+    });
+
+};
---
+++ Server/config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url': 'mongodb://deadbodies:deadbodies@ds035702.mongolab.com:35702/deadbodies'
+
+};
