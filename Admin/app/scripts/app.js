5baa26bf17ccdeb7bbf611047f2bafcdb7fac42d
Omar Ansari <ansario@email.sc.edu>
2016-01-23 21:56:13 -0500

    Updating file limit

--- Admin/app/scripts/app.js
+++ Admin/app/scripts/app.js
@@ -23,9 +23,9 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller: 'AdminCtrl'
       })
       .when('/login', {
         templateUrl: 'views/login.html',
