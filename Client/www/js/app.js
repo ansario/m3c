a3d9a773b18b7d040090f775b923a916c5f21f49
Blakeley Hoffman <blakeleyhoffman@gmail.com>
2016-01-23 21:13:08 -0500

    material design

--- Client/www/js/app.js
+++ Client/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova', 'ngMap'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova', 'ngMap', 'ionic-material'])
 
 .run(function($ionicPlatform, $cordovaGeolocation) {
   $ionicPlatform.ready(function() {
